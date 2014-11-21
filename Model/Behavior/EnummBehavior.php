b386feeccd2c3df8a40639e07af1e63dda1fb973
kaihiro <kai@fusic.co.jp>
2014-11-21 21:10:10 +0900

    Change Behavior for Cakeplus ValidationPatterns

--- Model/Behavior/EnummBehavior.php
+++ Model/Behavior/EnummBehavior.php
@@ -17,6 +17,12 @@ class EnummBehavior extends ModelBehavior {
             if (!is_array($values)) {
                 $values = Configure::read($values);
             }
+            // for Cakeplus
+            $validateRules = $model->validate[$fieldName];
+            if (isset($validateRules) && !is_array($validateRules)){
+                $model->validate[$fieldName] = [$validateRules];
+            }
+
             $model->validate[$fieldName]['inListEnumValidate'] = array(
                 'rule' => array('inList', array_keys($values)),
                 'allowEmpty' => true,
